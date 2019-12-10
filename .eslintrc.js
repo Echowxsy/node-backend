a8b68323e40589fb6942b89346dbfb94091ac1ec
Echowxsy <echowxsy@gmail.com>
2019-12-10 19:19:00 +0800

    chore: add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,21 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: ['plugin:@typescript-eslint/recommended', 'standard'],
+  extends: ['prettier/@typescript-eslint', 'plugin:prettier/recommended'],
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   env: {
     browser: true,
-    node: true
-  }
+    node: true,
+  },
 }
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 80,
+  endOfline: 'auto',
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: false,
+  arrowParens: 'avoid',
+  tabWidth: 2,
+  useTabs: false,
+}
